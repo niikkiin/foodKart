9aaaf897679afd5929b6101867c59c9e281ef6e2
Nikki <abarcanikki@gmail.com>
2020-07-15 21:05:25 +0800

    modified sign up and README.md

--- client/README.md
+++ client/README.md
@@ -40,7 +40,7 @@ Before you continue, ensure you have met the following requirements:
 
 * Install dependencies by running `yarn` or `npm install`.
 * Run `yarn start` or `npm run start` to start the local development server.
-* 😎 **That's it!** You're ready to start building awesome dashboards.
+* 😎 **That's it!** You're ready to start using FoodKart.
 
 <br />
 
--- client/src/views/signup.views.jsx
+++ client/src/views/signup.views.jsx
@@ -3,34 +3,25 @@ import React from "react";
 import { Container, Row, Col } from "shards-react";
 
 // svg for sign up
-import { ReactComponent as SignUpSVG } from '../assets/register/signUpSVG.svg';
+import { ReactComponent as SignUpSVG } from "../assets/register/signUpSVG.svg";
 import RegisterAccountDetails from "../components/registration/register.component";
 
 const SignUpView = () => (
-  <Container fluid className="main-content-container bg-white">
-    <Row className='m-5'>
-      <Col
-        lg="8"
-        md="12"
-        sm="12"
-      ><SignUpSVG /></Col>
-      <Col
-        lg="4"
-        md="12"
-        sm="12"
-      >
-        <h2 className='text-center'>Sign Up</h2>
+  <Container fluid className="bg-white">
+    <Row>
+      <Col xl="8" lg="8" md="8" sm="12" xs="12">
+        <SignUpSVG />
+      </Col>
+      <Col xl="4" lg="4" md="4" sm="12" xs="12">
+        <h3 className="text-center font-weight-bolder">Sign Up to FoodKart</h3>
         <RegisterAccountDetails />
       </Col>
     </Row>
   </Container>
 );
 
-SignUpView.propTypes = {
+SignUpView.propTypes = {};
 
-};
-
-SignUpView.defaultProps = {
-};
+SignUpView.defaultProps = {};
 
 export default SignUpView;
