3676b6e19e377d5755fc453994e20461a2275228
Nikki <abarcanikki@gmail.com>
2020-07-16 19:34:57 +0800

    Updates

---
+++ precache-manifest.cf72c76715614979e3cae38225a9b631.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a0433881c643426ed7",
+    "url": "/foodKart/static/css/main.df348cc0.chunk.css"
+  },
+  {
+    "revision": "a0a0433881c643426ed7",
+    "url": "/foodKart/static/js/main.a0a04338.chunk.js"
+  },
+  {
+    "revision": "12ab8484596db46ced33",
+    "url": "/foodKart/static/css/1.4a96cf2b.chunk.css"
+  },
+  {
+    "revision": "12ab8484596db46ced33",
+    "url": "/foodKart/static/js/1.12ab8484.chunk.js"
+  },
+  {
+    "revision": "8e18ec5b89e78d747842",
+    "url": "/foodKart/static/js/runtime~main.8e18ec5b.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/foodKart/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "4d6c1dd059b366464d769f42cf456d79",
+    "url": "/foodKart/static/media/temporary_logo.4d6c1dd0.svg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/foodKart/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/foodKart/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/foodKart/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/foodKart/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/foodKart/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/foodKart/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/foodKart/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/foodKart/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/foodKart/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/foodKart/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "de6d51b8f7626c77534646efe096c371",
+    "url": "/foodKart/static/media/signUpSVG.de6d51b8.svg"
+  },
+  {
+    "revision": "ae7b5730154cbe8456e29ee2e67290a8",
+    "url": "/foodKart/index.html"
+  }
+];
