d22d25e44957c5894e5afd2a363b5647a8a72348
Tito-Chu <freelance.jason24608@gmail.com>
2020-08-08 21:11:59 +0800

    login v2

--- client/src/components/registration/login.component.jsx
+++ client/src/components/registration/login.component.jsx
@@ -4,76 +4,71 @@ import {
   ListGroup,
   ListGroupItem,
   Row,
-  Col
-  //Form,
-  //FormInput,
-  //Button
+  Col,
+  Form,
+  FormInput,
+  Button
 } from "shards-react";
+import { OrGroup } from "../common/or.component";
 
 const LoginAccountDetails = ({ title }) => (
   <>
     <ListGroup flush>
       <ListGroupItem className="p-3">
-        <label htmlFor="">Sign In To FoodKart App</label>
         <Row>
-          <Col>
-            <Row form>
-              <Col md="12" className="form-group">
-                <button className="btn btn-danger form-control">
-                  Sign In with Google{" "}
-                </button>
-              </Col>
-            </Row>
+          <Col md='12'>
+            <Button className='btn-block' theme="danger">Sign in with Google</Button>
+            <Button className='btn-block' theme="primary">Sign in with Facebook</Button>
+          </Col>
+          <Col md='12'>
+            <OrGroup />
+          </Col>
+          <Col md='12'>
+            <Form>
 
-            <Row form>
-              <Col md="12" className="form-group">
-                <button className="btn btn-info form-control">
-                  Sign In With Facebook
-                </button>
-              </Col>
-            </Row>
+              <Row form>
+                {/* Email */}
+                <Col md="12" className="form-group">
+                  <label htmlFor="feEmail">Email</label>
+                  <FormInput
+                    type="email"
+                    id="feEmail"
+                    placeholder="Email Address"
+                    value="json@test.com"
+                    onChange={() => { }}
+                    autoComplete="email"
+                  />
+                </Col>
+              </Row>
+              <Row form>
+                {/* Password */}
+                <Col md="12" className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <FormInput
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    value="EX@MPL#P@$$w0RD"
+                    onChange={() => { }}
+                    autoComplete="current-password"
+                  />
+                </Col>
+              </Row>
+              <Row form className='d-flex align-content-center justify-content-center'>
+                <Col md="12">
+                  <p><input type="checkbox" /> Always Keep logged in</p>
+                </Col>
+                <Col className='text-center' md="12">
+                  <Button theme="accent">Login</Button>
+                </Col>
 
-            <Row form>
-              <Col md="12" className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value="json1218@gmail.com"
-                />
-              </Col>
-            </Row>
-            <Row form>
-              <Col md="12" className="form-group">
-                <label htmlFor="email">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="email"
-                  value="json1218@gmail.com"
-                />
-              </Col>
-            </Row>
-            <Row form>
-              <Col md="12" className="form-group">
-                <input type="checkbox" /> Always keep logged in
-              </Col>
-            </Row>
-
-            <Row form>
-              <Col md="12" className="form-group">
-                <button className="btn btn-info">Login</button>
-              </Col>
-            </Row>
-
-            <Row form>
-              <Col md="12" className="form-group">
-                <label htmlFor="">
-                  Don't have an account? <a href="/register">Sign up here.</a>
-                </label>
-              </Col>
-            </Row>
+              </Row>
+              <Row form className='d-flex align-content-center justify-content-center'>
+                <Col md="12">
+                  <p>Don't have an account? Click <a href="/register">Here</a></p>
+                </Col>
+              </Row>
+            </Form>
           </Col>
         </Row>
       </ListGroupItem>
